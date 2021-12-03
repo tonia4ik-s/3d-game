d1507edff9cfd53fcaace99c103d0897ad10507a
tonia4ik-s <cooltone4ka@gmail.com>
2021-12-04 00:46:51 +0200

    Creating game

---
+++ 3dGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraController : MonoBehaviour
+{
+    private float _mouseX;
+    private float _mouseY;
+
+    [Header("Sensitivity mouse")]
+    public float sensitivityMouse = 200f;
+
+    public bool invertedCameraRotation = false;
+
+    public Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mouseX = Input.GetAxis("Mouse X") * sensitivityMouse * Time.deltaTime;
+        _mouseY = Input.GetAxis("Mouse Y") * sensitivityMouse * Time.deltaTime;
+        
+        var transform1 = transform;
+        player.Rotate(_mouseX * new Vector3(0, 1, 0));
+        if (!invertedCameraRotation)
+        {
+            transform1.Rotate(_mouseX * new Vector3(0, 1, 0));
+            transform1.Rotate(-_mouseY * new Vector3(1, 0, 0));
+        }
+        else
+        {
+            transform1.Rotate(-_mouseX * new Vector3(0, 1, 0));
+            transform1.Rotate(+_mouseY * new Vector3(1, 0, 0));
+        }
+        
+        transform1.localPosition = player.position;
+    }
+}
---
+++ 3dGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player speed")]
+    public float speed = 7f;
+    
+    public float runSpeed = 15f;
+
+    public float jumpPower = 200f;
+
+    public bool isOnTheGround;
+
+    public Rigidbody rb;
+    
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        var transform1 = transform;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform1.localPosition += transform1.forward * runSpeed * Time.deltaTime;
+            }
+            else
+            {
+                transform1.localPosition += transform1.forward * speed * Time.deltaTime;
+            }
+        }
+        
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform1.localPosition += -transform1.forward * runSpeed * Time.deltaTime;
+            }
+            else
+            {
+                transform1.localPosition += -transform1.forward * speed * Time.deltaTime;
+            }
+        }
+        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform1.localPosition += -transform1.right * runSpeed * Time.deltaTime;            
+            }
+            else
+            {
+                transform1.localPosition += -transform1.right * speed * Time.deltaTime;            
+            }
+        }
+        
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform1.localPosition += transform1.right * runSpeed * Time.deltaTime;
+            }
+            else
+            {
+                transform1.localPosition += transform1.right * speed * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOnTheGround)
+            {
+                rb.AddForce(transform.up * jumpPower);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag($"Ground"))
+        {
+            isOnTheGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag($"Ground"))
+        {
+            isOnTheGround = false;
+        }
+    }
+}
